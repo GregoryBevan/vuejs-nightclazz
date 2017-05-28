81bb9dabc88cbe2c1e4ec39cf947b899f2a626af
gregory <gregory_bevan@hotmail.com>
2017-05-28 21:19:41 +0200

    Objectif 2 : Créer le composant Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- Component html code here -->
+  <p>{{ series }}</p>
+</template>
+
+<script>
+// Add imports here
+export default {
+  // Component definition
+  data () {
+    return {
+      series: 'Liste des séries'
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
